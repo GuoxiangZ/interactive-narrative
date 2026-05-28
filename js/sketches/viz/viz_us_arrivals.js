7d4fd68e0e8ec658aaf905c893993dc3d09fa000
JiufuZh <zjfu2003@gmail.com>
2026-05-28 00:43:23 -0700

    Adjust arrivals door sign position

--- js/sketches/viz/viz_us_arrivals.js
+++ js/sketches/viz/viz_us_arrivals.js
@@ -536,9 +536,9 @@
     }
 
     function drawCleanArrivalDoorSign(p, x, y, w, h) {
-        var sx = x + w * 0.482;
-        var sy = y + h * 0.515;
-        var sw = w * 0.082;
+        var sx = x + w * 0.51;
+        var sy = y + h * 0.56;
+        var sw = w * 0.085;
         var sh = h * 0.038;
         p.push();
         p.noStroke();
